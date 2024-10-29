833767ee606d838b81b35d7221f0019746c2895b
champst2004 <st.is.champion@gmail.com>
2024-10-29 23:01:14 +0530

    -

--- merge_sorted_arrays.py
+++ merge_sorted_arrays.py
@@ -1,13 +1,14 @@
 # https://leetcode.com/problems/merge-sorted-array/description/
 
 class Solution:
-    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
         i = m - 1
         j = n - 1
-        k = len(nums1) - 1
+        k = len(nums1) -1
+
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -22,6 +23,6 @@ class Solution:
             i -= 1
             k -= 1
         while j >= 0:
-            nums1[k] = nums2[j] 
+            nums1[k] = nums2[j]
             j -= 1
             k -= 1
