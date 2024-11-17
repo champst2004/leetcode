f047faf1d641fb424ebf81acaaf26bb100fcf26a
Shivank Tripathi <st.is.champion@gmail.com>
2024-11-17 22:33:32 +0530

    9

---
+++ palindrome_number.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = str(x)
+        if num == num[::-1]:
+            return True
+        return False
