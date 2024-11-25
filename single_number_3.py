95b0e870be2ea47c0af053fb2106a95c0057fb52
Shivank Tripathi <st.is.champion@gmail.com>
2024-11-25 14:53:28 +0530

    change

--- single_number_3.py
+++ single_number_3.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/single-number-iii/
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
+    def singleNumber(self, nums: list[int]) -> list[int]:
         dict = {}
         lst = []
         for i in range(len(nums)):
