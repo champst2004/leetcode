0c39a02ba56262af935552fe70931a9df2a913c2
Shivank Tripathi <st.is.champion@gmail.com>
2025-04-20 13:32:44 +0530

    day 23

---
+++ 2824.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+
+class Solution {
+    public:
+        int countPairs(vector<int>& nums, int target) {
+            int n = nums.size(), ans = 0;
+                    for(int i = 0; i < n; i++){
+                        for(int j = i + 1; j < n; j++){
+                            if(nums[i] + nums[j] < target) ans++;
+                        }
+                    }
+                    return ans;
+            }
+};
