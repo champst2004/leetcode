c0b8c829cc246dd5ecb541b2068770dd0fcb59bc
Shivank Tripathi <st.is.champion@gmail.com>
2025-05-04 22:52:06 +0530

    done 9 in cpp

--- 9.py
+++ 9.py
@@ -6,3 +6,19 @@ class Solution:
         if num == num[::-1]:
             return True
         return False
+    
+# CPP
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x == 0) return true;
+        string str1 = to_string(x), str2 = "";
+        int temp;
+        while(x > 0){
+            str2 += x % 10 + '0';
+            x = x/10;
+        }
+        return str1 == str2;
+    }
+};
