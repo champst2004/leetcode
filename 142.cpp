74dd99a10a34cccf90da9f329aa7082a73a342f5
Shivank Tripathi <st.is.champion@gmail.com>
2025-08-08 20:14:57 +0530

    day 59

---
+++ 142.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return fast;
+            }
+        }
+        return nullptr;
+    }
+};
